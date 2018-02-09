da966f99eb20c99eca2cab2b04710eb89dc54043
TimothyJohnK <Tim@VectorLab.com>
2018-02-09 12:10:04 -0600

    updated deps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import styled, { css } from 'styled-components';
 
-const Header = styled.div`
-  
-`
+const Header = styled.div``;
 
 export default class App extends Component {
   render() {
-    return <div> <Header /> </div>;
+    return (
+      <div>
+        {' '}
+        <Header />{' '}
+      </div>
+    );
   }
 }
